ccd691c47043c871f152e8280d5200d2b97dfd4b
AndrusovaK <andrusova_ksenia@mail.ru>
2015-11-07 15:50:54 +0300

    All finished (осталось доделать плашки new, sale)

---
+++ css/script.js
@@ -0,0 +1,11 @@
+$('input[type="date"]').each(function() {
+    var el = this, type = $(el).attr('type');
+    if ($(el).val() == '') $(el).attr('type', 'text');
+    $(el).focus(function() {
+        $(el).attr('type', type);
+        el.click();
+    });
+    $(el).blur(function() {
+        if ($(el).val() == '') $(el).attr('type', 'text');
+    });
+});
